8ecf556f6dbec4210bf7e1155886406577839012
unknown <beyondj.kim@gmail.com>
2020-09-27 16:49:06 +0900

    Add multiple customers

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { render } from '@testing-library/react';
+import Customer from './components/Customer';
 
+const customers =[{
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/1',
+  'name': '김지영',
+  'birthday': '910130',
+  'gender': '남자',
+  'job':'대학생'
+},
+{
+  'id': 2,
+  'image': 'https://placeimg.com/64/64/2',
+  'name': '김지영2',
+  'birthday': '9101302',
+  'gender': '남자2',
+  'job':'대학생2'
+}]
 class App extends Component {
   render(){
-    return (
-      <div className="gray-background">
-       <img src={logo} lat = "logo"/>
-       <h2>Let's develop management system!</h2>
+    return(
+      <div>
+        {
+          customers.map(c=>{
+            return(
+              <Customer 
+                key={c.id}
+                id={c.id}
+                image={c.image} 
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+            );
+          })
+        }
       </div>
-    );
+    ) 
   }
-  
 }
+  
+
 
 export default App;
